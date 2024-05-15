186dd4e54142a475f091a3b995afba999b2c3fb0
mangat-ram <se.mangat.ram@gmail.com>
2024-05-15 23:07:10 +0530

    added the Heading.

---
+++ src/app/(marketing)/_components/Heading.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1>
+        Your Ideas, Documents, & Plans.Unified. Welcome to <span className="underline">HyveNode</span>
+      </h1>
+    </div>
+  )
+}
+
+export default Heading
--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Heading from "./_components/Heading";
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+      </div>
+    </div>
   )
 }
 
-export default LandingPage;
+export default LandingPage
