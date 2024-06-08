662b69aca8dc2e5cc9d9d5cff0190d39c74d1e98
mangat-ram <se.mangat.ram@gmail.com>
2024-06-08 12:57:27 +0530

    Added Dialog Box.

--- src/app/(main)/_components/trashBox.tsx
+++ src/app/(main)/_components/trashBox.tsx
@@ -9,6 +9,7 @@ import { toast } from "sonner";
 import { Spinner } from "@/components/spinner";
 import { Search, Trash, Undo } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import { ConfirmModal } from "@/components/modals/confirmModal";
 
 export const TrashBox = () => {
 
@@ -43,10 +44,8 @@ export const TrashBox = () => {
   };
 
   const onRemove = (
-    event : React.MouseEvent<HTMLDivElement, MouseEvent>,
     documentId : Id<"documents">
   ) => {
-    event.stopPropagation();
     const promise = remove({id : documentId});
 
     toast.promise( promise,{
@@ -102,12 +101,14 @@ export const TrashBox = () => {
                 >
                   <Undo className="h-4 w-4 text-muted-foreground" />
                 </div>
-                <div
-                  role="button"
-                  className="rounded-sm p-2 hover:bg-neutral-200"
-                >
-                  <Trash className="h-4 w-4 text-muted-foreground" />
-                </div>
+                <ConfirmModal onConfirm={() => onRemove(document._id)}>
+                  <div
+                    role="button"
+                    className="rounded-sm p-2 hover:bg-neutral-200"
+                  >
+                    <Trash className="h-4 w-4 text-muted-foreground" />
+                  </div>
+                </ConfirmModal>
               </div>
             </div>    
           ))
