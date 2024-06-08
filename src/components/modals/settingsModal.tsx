414235abcbb7638263b8df45d642d155244dc702
mangat-ram <se.mangat.ram@gmail.com>
2024-06-08 16:55:04 +0530

    Added Settings Modal

--- src/components/modals/settingsModal.tsx
+++ src/components/modals/settingsModal.tsx
@@ -7,4 +7,30 @@ import {
 } from "@/components/ui/dialog"
 
 import { useSettings } from "@/hooks/useSettings";
-import 
+import { Label } from "../ui/label";
+
+export const SettingsModal = () => {
+  const settings = useSettings();
+
+  return (
+    <Dialog open={settings.isOpen} onOpenChange={settings.onClose}>
+      <DialogContent>
+        <DialogHeader className="border-b pb-3">
+          <h2 className="text-lg font-medium">
+            My Settings
+          </h2>
+        </DialogHeader>
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-y-1">
+            <Label>
+              Appearance
+            </Label>
+            <span>
+              Customize how HyveNode looks on your device
+            </span>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
