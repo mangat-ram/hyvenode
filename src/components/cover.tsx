d9a05c51a350583c59d5cd30e776dea8f451fb35
mangat-ram <se.mangat.ram@gmail.com>
2024-06-09 17:30:45 +0530

    Added Cover Skeleton and Replace cover image.

--- src/components/cover.tsx
+++ src/components/cover.tsx
@@ -3,6 +3,15 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
+import { Button } from "@/components/ui/button";
+import { ImageIcon, X } from "lucide-react";
+import { useCoverImage } from "@/hooks/useCoverImage";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "../../convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface CoverProps{
   url? : string;
@@ -13,6 +22,24 @@ export const Cover = ({
   url,
   preview
 }: CoverProps) => {
+
+  const { edgestore } = useEdgeStore();
+  const params      = useParams();
+  const coverImage  = useCoverImage();
+  const removeImage = useMutation(api.documents.removeCoverImage);
+
+  const onRemove = async () => {
+    if(url) {
+      await edgestore.publicFiles.delete({
+        url: url
+      })
+    }
+
+    removeImage({
+      id: params.documentId as Id<"documents">
+    })
+  }
+
   return (
     <div className={cn(
       "relative w-full h-[35vh] group",
@@ -27,6 +54,34 @@ export const Cover = ({
           className="object-cover"
         />
       )}
+      {url && !preview && (
+        <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+          <Button
+            onClick={() => coverImage.onReplace(url)}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+            Change Cover
+          </Button>
+          <Button
+            onClick={onRemove}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <X className="h-4 w-4 mr-2" />
+            Remove Cover
+          </Button>
+        </div>
+      )}
     </div>
+  )
+}
+
+Cover.Skeleton = function CoverSkeleton() {
+  return (
+    <Skeleton className="w-full h-[12vh]" />
   )
 }
