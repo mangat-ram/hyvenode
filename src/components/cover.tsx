e37247e982c38395cd94dd348de323eb32e031e3
mangat-ram <se.mangat.ram@gmail.com>
2024-06-09 16:22:59 +0530

    Added Cover

---
+++ src/components/cover.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import React from "react";
+
+interface CoverProps{
+  url? : string;
+  preview? : boolean;
+}
+
+export const Cover = ({
+  url,
+  preview
+}: CoverProps) => {
+  return (
+    <div className={cn(
+      "relative w-full h-[35vh] group",
+      !url && "h-[12vh]",
+      url && "bg-muted"
+    )} >
+      {!!url && (
+        <Image
+          src={url}
+          fill
+          alt="Cover"
+          className="object-cover"
+        />
+      )}
+    </div>
+  )
+}
--- src/components/modals/coverImageModal.tsx
+++ src/components/modals/coverImageModal.tsx
@@ -6,9 +6,47 @@ import {
   DialogHeader,
 } from "@/components/ui/dialog";
 import { useCoverImage } from "@/hooks/useCoverImage";
+import { SingleImageDropzone } from "@/components/singleImageDropdown";
+import { useState } from "react";
+import { useEdgeStore } from "@/lib/edgestore";
+import { useMutation } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "../../../convex/_generated/dataModel";
 
 export const CoverImageModal = () => {
+  
+  const params = useParams();
+  const update = useMutation(api.documents.update);
   const coverImage = useCoverImage();
+  const { edgestore } = useEdgeStore();
+
+  const [file, setFile] = useState<File>();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onClose = () => {
+    setFile(undefined);
+    setIsSubmitting(false);
+    coverImage.onClose();
+  }
+
+  const onChange = async (file?: File) => {
+    if(file){
+      setIsSubmitting(true);
+      setFile(file);
+
+      const res = await edgestore.publicFiles.upload({
+        file
+      });
+
+      await update({
+        id: params.documentId as Id<"documents">,
+        coverImage: res.url
+      })
+
+      onClose();
+    }
+  }
 
   return (
     <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose} >
@@ -18,9 +56,12 @@ export const CoverImageModal = () => {
             Cover Image
           </h2>
         </DialogHeader>
-        <div>
-          TODO: Upload Image
-        </div>
+        <SingleImageDropzone 
+          className="w-full outline-none"
+          disabled={isSubmitting}
+          value={file}
+          onChange={onChange}
+        />
       </DialogContent>
     </Dialog>
   );
