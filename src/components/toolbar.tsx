070855003fdd7abac5a1ef371538304718f520b3
mangat-ram <se.mangat.ram@gmail.com>
2024-06-09 13:44:39 +0530

    Added Icon Picker

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,7 +1,12 @@
 "use client";
 
-import React from "react"
+import React, { ElementRef, useRef, useState } from "react"
 import { Doc } from "../../convex/_generated/dataModel";
+import { IconPicker } from "./iconPicker";
+import { Button } from "./ui/button";
+import { ImageIcon, Smile, X } from "lucide-react";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 interface ToolbarProps{
   initialData: Doc<"documents">;
@@ -12,13 +17,91 @@ export const Toolbar = ({
   initialData,
   preview
 } : ToolbarProps) => {
+
+  const inputRef = useRef<ElementRef<"textarea">>(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [value, setValue] = useState(initialData.title);
+
+  const update = useMutation(api.documents.update)
+  const enableInput = () => {
+    if(preview) return;
+
+    setIsEditing(true);
+    setTimeout(() => {
+      setValue(initialData.title);
+      inputRef.current?.focus();
+    }, 0);
+  };
+
+  const disableInput = () => setIsEditing(false);
+
+  const onInput = (value: string) => {
+    setValue(value);
+    update({
+      id: initialData._id,
+      title: value || "Unititled"
+    });
+  };
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if(event.key === "Enter"){
+      event.preventDefault();
+      disableInput();
+    }
+  };
+ 
   return (
     <div className="pl-[54px] group relative" >
       {!!initialData.icon && !preview && (
         <div className="flex items-center gap-x-2 group/icon pt-6" >
-          
+          <IconPicker onChange={() => {}}>
+            <p className="text-6xl hover:opacity-75 transition">
+              {initialData.icon}
+            </p>
+          </IconPicker>
+          <Button
+            onClick={() => {}}
+            className="rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs"
+            variant="outline"
+            size="icon"
+          >
+            <X className="h-4 w-4" />
+          </Button>
         </div>
       )}
+      {!!initialData.icon && preview && (
+        <p className="text-6xl pt-6">
+          {initialData.icon}
+        </p>
+      )}
+      <div className="opacity-100 group-hover:opacity-100 flex items-center gap-x-1 py-4">
+        {!initialData.icon && !preview && (
+          <IconPicker asChild onChange={() => {}}>
+            <Button 
+              className="text-muted-foreground text-xs"
+              variant="outline"
+              size="sm"
+            >
+              <Smile className="h-4 w-4 mr-2" />
+              Add Icon
+            </Button>
+          </IconPicker>
+        )}
+        {!initialData.coverImage && !preview && (
+          <Button
+            onClick={() => {}}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+            Add Cover Image
+          </Button>
+        )}
+      </div>
+      {isEditing && !preview ? (
+        
+      ) : ()}
     </div>
   )
 }
